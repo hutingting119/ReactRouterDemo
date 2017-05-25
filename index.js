677638750b000eba83c47c99fd2d6bb2faa967c9
hutingting119 <1912257350@qq.com>
2017-05-25 21:02:21 +0800

    add more route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/APP';
+import About from './modules/About';
+import Repos from './modules/Repos';
 
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}/>
+        <Route path='/about' component={About}/>
+        <Route path='repos' component={Repos}/>
     </Router>
 ), document.getElementById('app'));
