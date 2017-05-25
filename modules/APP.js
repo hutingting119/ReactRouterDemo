cb7e3da87409872cb26011358d0db6d50e0418a4
hutingting119 <1912257350@qq.com>
2017-05-25 21:11:20 +0800

    add link

--- modules/APP.js
+++ modules/APP.js
@@ -1,12 +1,20 @@
 import React  from 'react';
+import {Link} from 'react-router';
 
-export default class APP extends React.Component{
-    constructor(props){
+export default class APP extends React.Component {
+    constructor(props) {
         super(props);
     }
-    render(){
-        return(
-            <h1>React Router Demo</h1>
+
+    render() {
+        return (
+            <div>
+                <h1>React Router Demo</h1>
+                <ul role="nav">
+                    <li><Link to="/about"/>About</li>
+                    <li><Link to="repos"/>Repos</li>
+                </ul>
+            </div>
         )
     }
 }
